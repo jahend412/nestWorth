afa02e54b34f07272e35e6316152f69681569730
Josh Endemann <jahend44@gmail.com>
2025-05-21 18:59:07 -0500

    Update authRoutes.js
    import protect and restrictTo for auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,19 @@
 import express from "express";
-import { login, signup } from "../controllers/authController";
+import {
+  login,
+  signup,
+  protect,
+  restrictTo,
+} from "../controllers/authController";
 
 const router = express.Router();
+
+router.get("/admin-only", protect, restrictTo("admin"), (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "This route is only for admins!",
+  });
+});
 
 router.post("/signup", signup);
 router.post("/login", login);
