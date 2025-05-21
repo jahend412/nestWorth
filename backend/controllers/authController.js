7bcf8dac98148b987ca426b3908154f9347770bc
Josh Endemann <jahend44@gmail.com>
2025-05-21 15:13:10 -0500

    Add a protection middleware
    - This ensures that the routes are protected from unauthorized access

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/index";
 import catchAsync from "../utils/catchAsync";
 import AppError from "../utils/appError";
+import { promisify } from "util";
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -48,6 +49,8 @@ export const signup = catchAsync(async (req, res) => {
       },
     },
   });
+
+  createSendToken(newUser, 201, res);
 });
 
 export const login = catchAsync(async (req, res, next) => {
@@ -82,4 +85,47 @@ export const login = catchAsync(async (req, res, next) => {
       },
     },
   });
+
+  createSendToken(user, 200, res);
+});
+
+// Middleware to protect routes from unauthorized access
+
+export const protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access.", 401)
+    );
+  }
+
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findByPk(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError("The user belonging to this token no longer exists.", 401)
+    );
+  }
+
+  // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError("User recently changed password! Please log in again.", 401)
+    );
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  next();
 });
