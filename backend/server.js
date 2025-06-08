420caedbe8d8594b6de2beeb028692accff285af
Josh Endemann <jahend44@gmail.com>
2025-06-08 11:58:07 -0500

    Changed server port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import http from "http";
 import process from "process";
 
 // Set port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Create HTTP server
 const server = http.createServer(app);
