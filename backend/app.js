062184d57756472f7f83ee38b9e1ffbdd4acc3df
Josh Endemann <jahend44@gmail.com>
2025-06-08 12:00:42 -0500

    Fixed path-to-regexp error.
    -  removed rate limiting to add to authRoutes.
    - Removed AppError
    - Changed app.all, to app.use.
    THe error was app.all was causing express's path to regexp parser to choke on a 'wildcard pattern'.  The solution to use app.use() with out a path is cleaner way to handle catch-all routes

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,9 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
-import rateLimit from "express-rate-limit";
 import xss from "xss-clean";
 import cookieParser from "cookie-parser";
 import hpp from "hpp";
-import AppError from "./utils/appError.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 // Route Imports
@@ -32,31 +30,6 @@ app.use(
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
-// Rate limiting = prevents brute force attacks
-const limiter = rateLimit({
-  max: 100, // 100 requests per hour
-  windowsMs: 60 * 60 * 1000, // 1 hour
-  message: {
-    status: "error",
-    message: "Too many requests from this IP, please try again in an hour!",
-  },
-  standardHeaders: true, // Return rate limit info in headrs
-  legacyHeaders: false,
-});
-app.use("/api", limiter);
-
-//  Stricter rate limiting for auth routes
-const authLimiter = rateLimit({
-  max: 10,
-  windowsMs: 15 * 60 * 1000,
-  message: {
-    status: "error",
-    message: "Too many authentication attempts, please try again later!",
-  },
-  skipSuccessfulRequests: true, // Don't count successful requests
-});
-app.use("/api/v1/auth", authLimiter);
 
 app.use(
   hpp({
@@ -93,8 +66,11 @@ app.use("/api/v1/accounts", accountRouter);
 app.use("/api/v1/transactions", transactionRoutes);
 
 // 3) HANDLE UNDEFINED ROUTES
-app.all("*", (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+app.use((req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: "Route not found",
+  });
 });
 
 // 4) GLOBAL ERROR HANDLING MIDDLEWARE
