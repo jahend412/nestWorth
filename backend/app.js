1dd8bcd349116711f8aa6f1222c6579bfb6c07d9
Josh Endemann <jahend44@gmail.com>
2025-06-10 18:56:47 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -52,18 +52,22 @@ app.use(
     origin:
       process.env.NODE_ENV === "production"
         ? process.env.FRONTEND_URL
-        : "http://localhost:3000",
+        : "http://localhost:5173",
     credentials: true, // Important for cookies
   })
 );
 
 // Serve static files (if needed)
 // app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`, req.body);
+  next();
+});
 
 // 2) ROUTES
-app.use("/api/auth", authRoutes);
-app.use("/api/accounts", accountRouter);
-app.use("/api/transactions", transactionRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/accounts", accountRouter);
+app.use("/api/v1/transactions", transactionRoutes);
 
 // 3) HANDLE UNDEFINED ROUTES
 app.use((req, res) => {
