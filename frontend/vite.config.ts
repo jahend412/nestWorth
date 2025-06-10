56ac762bfcf80d781b30e0f900d79b158a683cc0
Josh Endemann <jahend44@gmail.com>
2025-06-10 18:16:45 -0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:8080", // Change from 3000 to 8080
+      "/api": "http://localhost:8080",
     },
   },
 });
